74f4933cac91d5b8467841bdecd897e736752913
ble <ble.message@gmail.com>
2019-08-26 20:37:05 -0400

    initial commit with Heap Analytics

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const IndexPage: NextPage = () => {
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+
+
+      <Head>
+      {/* Heap Analytics */}
+      <script type="text/javascript">
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+      heap.load("3108805195");
+    </script>
+      </Head>
+
+
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default IndexPage;
